a871c26a49895d772fa311abc7e5f47001babc31
Erich Krueger <eckrueger42@gmail.com>
2025-01-24 15:58:52 -0600

    Reordered function code blocks to fix implicit declaration gcc warnings.

--- sprinter.c
+++ sprinter.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// TODO: refactor to have main at the bottom of the file.
-int main(int argc, char* argv[]) {
-	const int default_duration = 15;
-	int duration = (argc > 1) ? atoi(argv[1]) : default_duration;
-	(duration > 0) ? printf("Sprinting for %d minutes.\n", duration) : printf("Sprint duration must be greater than 0 minutes.\n");
-	
-	int starting_words = get_starting_words();
-	get_final_words(starting_words, duration);
-
-	return 0;
-}
 
 int get_starting_words() {
 	int starting_words;
 	printf("Enter starting wordcount:\n");
 	scanf("%d", &starting_words);
 	return starting_words;
+}
+
+
+int int_division_rounded(int dividend, int divisor) {
+	int remainder = dividend % divisor;
+	int result = dividend / divisor;
+	return (remainder > divisor / 2) ? result + 1 : result;
 }
 
 void get_final_words(int starting_words, int duration) {
@@ -28,9 +24,14 @@ void get_final_words(int starting_words, int duration) {
 	printf("Added %d words (%d wpm).\n", added_words, int_division_rounded(added_words, duration));
 }
 
-int int_division_rounded(int dividend, int divisor) {
-	int remainder = dividend % divisor;
-	int result = dividend / divisor;
-	return (remainder > divisor / 2) ? result + 1 : result;
+
+int main(int argc, char* argv[]) {
+	const int default_duration = 15;
+	int duration = (argc > 1) ? atoi(argv[1]) : default_duration;
+	(duration > 0) ? printf("Sprinting for %d minutes.\n", duration) : printf("Sprint duration must be greater than 0 minutes.\n");
+	
+	int starting_words = get_starting_words();
+	get_final_words(starting_words, duration);
+	return 0;
 }
 
