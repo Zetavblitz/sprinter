068355e9b564a00984588877f3667cf28b18dd8d
Erich Krueger <eckrueger42@gmail.com>
2025-01-24 12:53:12 -0600

    Added sprint duration as a program argument.

--- sprinter.c
+++ sprinter.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-	(argc > 1) ? printf("supplied args\n") : printf("no args\n");
+	int duration = (argc > 1) ? atoi(argv[1]) : 15;
+	(duration > 0) ? printf("Sprinting for %d minutes.\n", duration) : printf("Sprint duration must be greater than 0 minutes.\n");
 	return 0;
 }
 
