6b3c0f5268c91d41dc7104bd65b8316a0ac8ef65
Erich Krueger <eckrueger42@gmail.com>
2025-01-27 15:00:20 -0600

    Removed the dangerous use of scanf for getting user input. Now using fgets.

--- sprinter.c
+++ sprinter.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int get_starting_words() {
 	int starting_words;
-	printf("Enter starting wordcount:\n");
-	scanf("%d", &starting_words);
+	char buf[1024];
+
+	printf("Enter starting wordcount: ");
+	
+	if (!fgets(buf, 1024, stdin)) {
+		exit(1);
+	}
+
+	starting_words = atoi(buf);
 	return starting_words;
 }
 
@@ -18,12 +26,33 @@ int int_division_rounded(int dividend, int divisor) {
 
 void get_final_words(int starting_words, int duration) {
 	int final_words;
-	printf("Enter final wordcount:\n");
-	scanf("%d", &final_words);
+	char buf[1024];
+
+	printf("Enter final wordcount: ");
+
+	if (!fgets(buf, 1024, stdin)) {
+		exit(1);
+	}
+
+	final_words = atoi(buf);
 	int added_words = final_words - starting_words;
-	printf("Added %d words (%d wpm).\n", added_words, int_division_rounded(added_words, duration));
+	int wpm = int_division_rounded(added_words, duration);
+
+	printf("Added %d words (%d wpm).\n", added_words, wpm);
 }
 
+
+void listen() {
+	char* cancel = "cancel";
+
+	char command[24];
+	if (fgets(command, 24, stdin)) {
+		command[strcspn(command, "\n")] = 0;
+		if (strcmp(cancel, command) == 0) {
+			exit(0);
+		}
+	}
+}
 
 int main(int argc, char* argv[]) {
 	const int default_duration = 15;
@@ -31,6 +60,7 @@ int main(int argc, char* argv[]) {
 	(duration > 0) ? printf("Sprinting for %d minutes.\n", duration) : printf("Sprint duration must be greater than 0 minutes.\n");
 	
 	int starting_words = get_starting_words();
+	// listen();
 	get_final_words(starting_words, duration);
 	return 0;
 }
